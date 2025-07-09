6816d1bbd28572ef01f934a96a52e8669187ca73
yaskho <yassineholberton@gmail.com>
2025-07-10 00:14:07 +0100

    amenity file

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,30 +1,29 @@
-import uuid
-from datetime import datetime
+from typing import List
+from . import BaseModel
+from .place import Place
 
-class Amenity:
-    """Amenity model for HBnB application"""
-    
+class Amenity(BaseModel):
+    """Amenity model with relationships"""
     def __init__(self, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-        # Required attribute with validation
-        self.name = kwargs.get('name', '')
-        
-        if not self.name or len(self.name) > 50:
-            raise ValueError("Name is required and must be 50 characters or less")
+        super().__init__(**kwargs)
+        self.name: str = kwargs.get('name', '')
+        self._places: List[Place] = []
 
-    def save(self):
-        """Update the updated_at timestamp"""
-        self.updated_at = datetime.now()
+    # Relationship Methods
+    @property
+    def places(self) -> List[Place]:
+        return self._places
 
-    def to_dict(self):
-        """Convert amenity to dictionary"""
-        return {
-            'id': self.id,
-            'name': self.name,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat(),
-            '__class__': self.__class__.__name__
-        }
+    def add_place(self, place: Place):
+        if place not in self._places:
+            self._places.append(place)
+            if self not in place.amenities:
+                place.add_amenity(self)
+            self.save()
+
+    def remove_place(self, place: Place):
+        if place in self._places:
+            self._places.remove(place)
+            if self in place.amenities:
+                place.remove_amenity(self)
+            self.save()
