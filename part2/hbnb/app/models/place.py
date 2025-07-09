f94f19616503c9cba6c5e2b981115740bc961e99
yaskho <yassineholberton@gmail.com>
2025-07-09 16:12:29 +0100

    place model file

---
+++ part2/hbnb/app/models/place.py
@@ -0,0 +1,47 @@
+import uuid
+from datetime import datetime
+
+class Place:
+    """Place model for HBnB application"""
+    
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        self.created_at = kwargs.get('created_at', datetime.now())
+        self.updated_at = kwargs.get('updated_at', datetime.now())
+        self.user_id = kwargs.get('user_id', '')
+        self.name = kwargs.get('name', '')
+        self.description = kwargs.get('description', '')
+        self.number_rooms = kwargs.get('number_rooms', 0)
+        self.number_bathrooms = kwargs.get('number_bathrooms', 0)
+        self.max_guest = kwargs.get('max_guest', 0)
+        self.price_by_night = kwargs.get('price_by_night', 0)
+        self.latitude = kwargs.get('latitude', 0.0)
+        self.longitude = kwargs.get('longitude', 0.0)
+        
+        # Relationships
+        self.amenity_ids = []
+        self.review_ids = []
+
+    def save(self):
+        """Update the updated_at timestamp"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Convert place to dictionary"""
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'description': self.description,
+            'number_rooms': self.number_rooms,
+            'number_bathrooms': self.number_bathrooms,
+            'max_guest': self.max_guest,
+            'price_by_night': self.price_by_night,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'amenity_ids': self.amenity_ids,
+            'review_ids': self.review_ids,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            '__class__': self.__class__.__name__
+        }
