8a32af5991175f31175c8851bd22894eab91c269
yaskho <yassineholberton@gmail.com>
2025-07-14 00:18:47 +0100

    jhdg

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -16,14 +16,24 @@ amenity_model = api.model('Amenity', {
     'name': fields.String,
 })
 
+review_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
+
 place_model = api.model('Place', {
-    'title': fields.String(required=True),
-    'description': fields.String,
-    'price': fields.Float(required=True),
-    'latitude': fields.Float(required=True),
-    'longitude': fields.Float(required=True),
-    'owner_id': fields.String(required=True),
-    'amenities': fields.List(fields.String, required=True)
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'owner': fields.Nested(user_model, description='Owner of the place'),
+    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
+    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
+})
 })
 
 @api.route('/')
