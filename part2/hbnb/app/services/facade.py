ced9815917539de6cb4d1f5e52024d545910d1be
yaskho <yassineholberton@gmail.com>
2025-07-13 22:52:03 +0100

    fac

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,3 +1,4 @@
+from app.models.amenity import Amenity
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -28,17 +29,22 @@ class HBnBFacade:
         return user
     
     def create_amenity(self, amenity_data):
-    # Placeholder for logic to create an amenity
-    pass
+        if not amenity_data.get("name"):
+            raise ValueError("Amenity name is required")
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
     def get_amenity(self, amenity_id):
-    # Placeholder for logic to retrieve an amenity by ID
-    pass
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-    # Placeholder for logic to retrieve all amenities
-    pass
+        return self.amenity_repo.all()
 
     def update_amenity(self, amenity_id, amenity_data):
-    # Placeholder for logic to update an amenity
-    pass
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        if 'name' in amenity_data:
+            amenity.name = amenity_data['name']
+        return amenity
