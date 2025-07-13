d847d1dd7fa098eb505506fa888bc457f2998b97
yaskho <yassineholberton@gmail.com>
2025-07-13 22:46:38 +0100

    ament

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,29 +1,6 @@
-from typing import List
-from . import BaseModel
-from .place import Place
+from uuid import uuid4
 
-class Amenity(BaseModel):
-    """Amenity model with relationships"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name: str = kwargs.get('name', '')
-        self._places: List[Place] = []
-
-    # Relationship Methods
-    @property
-    def places(self) -> List[Place]:
-        return self._places
-
-    def add_place(self, place: Place):
-        if place not in self._places:
-            self._places.append(place)
-            if self not in place.amenities:
-                place.add_amenity(self)
-            self.save()
-
-    def remove_place(self, place: Place):
-        if place in self._places:
-            self._places.remove(place)
-            if self in place.amenities:
-                place.remove_amenity(self)
-            self.save()
+class Amenity:
+    def __init__(self, name, id=None):
+        self.id = id or str(uuid4())
+        self.name = name
