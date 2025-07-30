63f4c36aff03b3d5385b876c72e554aa0023a675
fahdchalladia <fahdchalladia2004@gmail.com>
2025-07-31 00:08:37 +0100

    auth namespace

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,9 +4,11 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth
 from flask_bcrypt import Bcrypt
 from app.extensions import bcrypt
 from flask_jwt_extended import JWTManager
+
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -20,6 +22,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth, path='/api/v1/auth')
     bcrypt.init_app(app)
     jwt.init_app(app)
     
