6b9cbe4e6bb15f7a278c32713e293a8c9d8debcd
fahdchalladia <fahdchalladia2004@gmail.com>
2025-07-30 18:07:28 +0100

    adding two methods

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -16,3 +16,6 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
