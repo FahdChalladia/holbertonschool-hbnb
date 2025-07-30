def11d2a6f9c901a56d00203ae9ab1ebddf7f782
yaskho <yassineholberton@gmail.com>
2025-07-30 22:23:20 +0100

    userf

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from app.extensions import bcrypt
 
 api = Namespace('users', description='User operations')
 
@@ -8,6 +9,7 @@ user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name'),
     'last_name': fields.String(required=True, description='Last name'),
     'email': fields.String(required=True, description='Email address')
+    'password': fields.String(required=True, description='Password', min_length=6)
 })
 
 @api.route('/')
@@ -21,6 +23,7 @@ class UserList(Resource):
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+        user_data['password'] = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
         new_user = facade.create_user(user_data)
         return {
             'id': new_user.id,
